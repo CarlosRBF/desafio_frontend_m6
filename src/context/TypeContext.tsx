fe4703a68ecc4512e06224ee934ebc550a71bf69
CarlosRBF <carlosrblanco00@gmail.com>
2023-02-07 11:15:55 -0300

    feat: refactor code

--- src/context/TypeContext.tsx
+++ src/context/TypeContext.tsx
@@ -1,19 +1,6 @@
 import { useEffect, useState, ReactNode } from "react";
 import { api } from "../services/request";
 
-// import * as yup from "yup"
-
-// let schema = yup.object().shape({
-//   amount: yup.number().required(),
-//   installments: yup.number().required(),
-//   mdr: yup.number().required(),
-// })
-
-// schema.isValid({
-//     amount: 1, 
-
-// })
-
 interface PropsData {
     children: ReactNode;
   }
